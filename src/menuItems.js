d9e6461cef25babf274010e99ee452a0bd080b3e
rtsng <ryan.j.tsang@gmail.com>
2022-03-26 23:29:24 -0400

    basic dropdown menu to start

---
+++ src/menuItems.js
@@ -0,0 +1,47 @@
+export const menuItems = [
+    {
+      title: "drop",
+    },
+    {
+      title: "down",
+      submenu: [
+        {
+          title: "menu",
+          link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        },
+        {
+          title: "wayaya",
+          submenu: [
+            {
+              title: "yaaaaaa",
+            },
+            {
+              title: "aya",
+              submenu: [
+                {
+                  title: "ya",
+                },
+                {
+                  title: "Pya",
+                },
+              ],
+            },
+          ],
+        },
+        {
+          title: "SEO",
+        },
+      ],
+    },
+    {
+      title: "menu",
+      submenu: [
+        {
+          title: "ligma",
+        },
+        {
+          title: "balls",
+        },
+      ],
+    },
+  ];
